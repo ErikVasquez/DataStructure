931684bcf464c12c3ef97e86db894f92dfee59d8
ErikVasquez <42862990+ErikVasquez@users.noreply.github.com>
2018-09-19 18:15:41 -0700

    Update NaturalNumbers.py

--- NaturalNumbers.py
+++ NaturalNumbers.py
@@ -1,7 +1,16 @@
 #DelgadoVasquezErik No.Control 17211515
-
 n=0
-for n in range (1,101, 1):
-    print (n)
+def NN(cien):
+    if cien <=100:
+        print (cien)
+        NN(cien+1)
+    else:
+        print("End")
+NN(n)
+    
+#Me equivoque de programa xd igual subí el otro, aunque tarde, y edito esto aqui, pero bueno, eso.
+#n=0
+#for n in range (1,101, 1):
+ #   print (n)
 
 

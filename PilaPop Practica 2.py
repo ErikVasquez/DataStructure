0f217f428e32233a2d48b7431621dab32514b9ca
ErikVasquez <42862990+ErikVasquez@users.noreply.github.com>
2018-10-08 13:35:45 -0700

    Add files via upload

---
+++ PilaPop Practica 2.py
@@ -0,0 +1,31 @@
+#Delgado Vasquez Erik No.Control 17211515
+v = 0
+ind = 0
+elec = ""
+stock =[]
+
+def push(ind, v):
+    stock.append(v)
+    return ind +1
+
+def peek(elec):
+    if elec =="T":
+        print (stock)
+    else:
+        elec = int(elec)
+        if elec>=0 and elec<5:
+            print (stock[elec])
+        else:
+            1
+            print ("Indice fuera de rango")
+while ind>=0 and ind<5:
+    v = int(input("Ingrese un valor entero (" +str(ind+1)+"/5): "))
+    ind = push(ind,v)
+
+def pop():
+    stock.pop()
+    return stock
+pop()
+
+elec = str(input("Teclee ""T"" Para mostras la lista o ingrese el indice del valor que desea ver(0 a 4): "))
+peek(elec)
